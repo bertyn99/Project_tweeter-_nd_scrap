7b09be97bd7ef093aecdcfce1d69ffa96273d9ee
FloLeh <florianlehuede@gmail.com>
2018-07-19 12:48:59 +0200

    tests

--- lib/app/towhall_mail.rb
+++ lib/app/towhall_mail.rb
@@ -1,12 +1,14 @@
 require 'gmail'
 require 'dotenv'
 Dotenv.load
+
+
 mail = ENV['MAIL']
 password = ENV['PASSWORD']
 gmail = Gmail.connect(mail,password)
 
 =begin
-gmail = Gmail.connect(:xoauth, "thp.acc.tests@gmail.com",
+gmail = Gmail.connect(:xoauth, mail,
   #:token           => ENV['TOKEN'],
   #:secret          => ENV['TOKEN_SECRET'],
   :consumer_key    => ENV['CONSUMER_KEY'],
@@ -16,8 +18,14 @@ gmail = Gmail.connect(:xoauth, "thp.acc.tests@gmail.com",
 
 email = gmail.compose do
   to "thp.acc.tests@gmail.com"
-  subject "1,2 1,2 this is a test"
-  body "Spent the day on the road..."
+  subject "Formation gratuite en programmation"
+  body "Bonjour,
+Je m'appelle Florian, je suis élève à The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique. La pédagogie de notre école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code. Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.
+
+Déjà 500 personnes sont passées par The Hacking Project. Est-ce que la mairie de [NOM_COMMUNE] veut changer le monde avec nous ?
+
+
+Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80"
 end
 email.deliver! # or: gmail.deliver(email)
 
