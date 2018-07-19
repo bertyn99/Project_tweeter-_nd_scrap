84eb0a7718fe955a99ea9bef7091f6738b6fd501
ruby <infinitydev4>
2018-07-19 07:13:39 -0700

    scrapping_mairies

---
+++ lib/app/townhall_scrapper.rb
@@ -0,0 +1,54 @@
+
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+class Scrapping
+ 
+def get_the_email_of_a_townhal_from_its_webpage(url)
+    page = Nokogiri::HTML(open(url))
+    
+	return   page.css('td')[7].text
+     
+end
+def get_the_CP_of_a_townhal_from_its_webpage(url)
+    page = Nokogiri::HTML(open(url))
+ 
+	return   page.css('td')[11].text
+end
+
+def get_all_the_urls_of_morbihan_townhalls(page_url)
+
+page = Nokogiri::HTML(open(page_url))
+ville = page.css('p>a').map {|ville| ville.text
+
+}
+ link = page.css('p>a').map {|vill| vill['href']} 
+return [ville,link]
+
+end
+
+
+
+
+def annuaire_mail_ville(page_url)
+	@hash = []
+	i=0
+	
+array = get_all_the_urls_of_morbihan_townhalls(page_url)
+	@villes=array[0]
+	@cp=array[1].map{ |url|
+	get_the_CP_of_a_townhal_from_its_webpage("http://www.annuaire-des-mairies.com/#{url}")}
+	@email = array[1].map{ |url|
+	get_the_email_of_a_townhal_from_its_webpage("http://www.annuaire-des-mairies.com/#{url}")}
+	@villes.length.times do
+		@hash += [{ "name" => @villes[i], "email" => @email[i], "cp" => @cp[i]}] 
+		i += 1
+	end
+	return @hash
+	end
+
+end
+var=Scrapping.new
+morbillan=Scrapping.new
+#puts scr.annuaire_mail_ville("http://www.annuaire-des-mairies.com/var.html")
